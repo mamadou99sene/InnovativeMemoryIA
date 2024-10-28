751c9010e8da6aac52c4ab17ec5f84f909aa6d35
Mamadou SENE <senemamadou1999@gmail.com>
2024-10-28 13:29:43 +0000

    commit du projet

---
+++ src/main/java/ucad/ia/innovativememoryia/InnovativeMemoryIaApplication.java
@@ -0,0 +1,13 @@
+package ucad.ia.innovativememoryia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovativeMemoryIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnovativeMemoryIaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ucad/ia/innovativememoryia/InnovativeMemoryIaApplicationTests.java
@@ -0,0 +1,13 @@
+package ucad.ia.innovativememoryia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovativeMemoryIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
